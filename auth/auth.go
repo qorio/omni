6913e5ed14cd6d19e2f971bd7556726dd3703d72
lab616 <dchung616@gmail.com>
2014-06-19 14:11:01 -0700

    JWT / token for auth

--- auth/auth.go
+++ auth/auth.go
@@ -1,110 +1,67 @@
 package auth
 
 import (
-	"bytes"
-	"crypto/cipher"
-	"crypto/rand"
-	// "crypto/subtle"
-	"encoding/base64"
-	"encoding/gob"
 	"errors"
-	//"fmt"
-	//"hash"
-	"io"
-	//"strconv"
-	//"time"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"time"
 )
 
 type UUID string
 
+type Auth struct {
+	signingKey []byte
+	TTLHours   time.Duration
+	GetTime    func() time.Time
+}
+
+var (
+	InvalidToken error = errors.New("invalid-token")
+	ExpiredToken error = errors.New("token-expired")
+)
+
+func NewAuth(key []byte) *Auth {
+	return &Auth{
+		signingKey: key,
+		TTLHours:   72,
+		GetTime:    func() time.Time { return time.Now() },
+	}
+}
+
 // Resolve from token to app key
-func GetAppKey(token string) (appKey UUID, err error) {
+func (this *Auth) GetAppKey(tokenString string) (appKey UUID, err error) {
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) ([]byte, error) {
+		return this.signingKey, nil
+	})
+
+	if err == nil && token.Valid {
+		// Check expiration
+		exp, ok := token.Claims["exp"].(float64)
+		if !ok {
+			err = InvalidToken
+			return
+		}
+
+		if this.GetTime().After(time.Unix(int64(exp), 0)) {
+			err = ExpiredToken
+			return
+		}
+
+		appKey = UUID(fmt.Sprintf("%s", token.Claims["appKey"]))
+		return
+	} else {
+		err = InvalidToken
+	}
 	return
 }
 
 // Given the app key, returns the token -- used during signup
-func GetAppToken(appKey UUID) (token string, err error) {
+func (this *Auth) GetAppToken(appKey UUID) (tokenString string, err error) {
+	token := jwt.New(jwt.GetSigningMethod("HS256"))
+	// Set some claims
+	token.Claims["appKey"] = appKey
+	token.Claims["exp"] = time.Now().Add(time.Hour * this.TTLHours).Unix()
+	// Sign and get the complete encoded token as a string
+	tokenString, err = token.SignedString(this.signingKey)
 	return
-}
-
-// encrypt encrypts a value using the given block in counter mode.
-//
-// A random initialization vector (http://goo.gl/zF67k) with the length of the
-// block size is prepended to the resulting ciphertext.
-func encrypt(block cipher.Block, value []byte) ([]byte, error) {
-	iv := GenerateRandomKey(block.BlockSize())
-	if iv == nil {
-		return nil, errors.New("securecookie: failed to generate random iv")
-	}
-	// Encrypt it.
-	stream := cipher.NewCTR(block, iv)
-	stream.XORKeyStream(value, value)
-	// Return iv + ciphertext.
-	return append(iv, value...), nil
-}
-
-// decrypt decrypts a value using the given block in counter mode.
-//
-// The value to be decrypted must be prepended by a initialization vector
-// (http://goo.gl/zF67k) with the length of the block size.
-func decrypt(block cipher.Block, value []byte) ([]byte, error) {
-	size := block.BlockSize()
-	if len(value) > size {
-		// Extract iv.
-		iv := value[:size]
-		// Extract ciphertext.
-		value = value[size:]
-		// Decrypt it.
-		stream := cipher.NewCTR(block, iv)
-		stream.XORKeyStream(value, value)
-		return value, nil
-	}
-	return nil, errors.New("securecookie: the value could not be decrypted")
-}
-
-// serialize encodes a value using gob.
-func serialize(src interface{}) ([]byte, error) {
-	buf := new(bytes.Buffer)
-	enc := gob.NewEncoder(buf)
-	if err := enc.Encode(src); err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
-}
-
-// deserialize decodes a value using gob.
-func deserialize(src []byte, dst interface{}) error {
-	dec := gob.NewDecoder(bytes.NewBuffer(src))
-	if err := dec.Decode(dst); err != nil {
-		return err
-	}
-	return nil
-}
-
-// Encoding -------------------------------------------------------------------
-
-// encode encodes a value using base64.
-func encode(value []byte) []byte {
-	encoded := make([]byte, base64.URLEncoding.EncodedLen(len(value)))
-	base64.URLEncoding.Encode(encoded, value)
-	return encoded
-}
-
-// decode decodes a cookie using base64.
-func decode(value []byte) ([]byte, error) {
-	decoded := make([]byte, base64.URLEncoding.DecodedLen(len(value)))
-	b, err := base64.URLEncoding.Decode(decoded, value)
-	if err != nil {
-		return nil, err
-	}
-	return decoded[:b], nil
-}
-
-// GenerateRandomKey creates a random key with the given strength.
-func GenerateRandomKey(strength int) []byte {
-	k := make([]byte, strength)
-	if _, err := io.ReadFull(rand.Reader, k); err != nil {
-		return nil
-	}
-	return k
 }
--- auth/auth_test.go
+++ auth/auth_test.go
@@ -1,9 +1,44 @@
 package auth
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestGetAppToken(t *testing.T) {
+
+	id := UUID("1234")
+
+	auth := NewAuth([]byte("test"))
+
+	token, err := auth.GetAppToken(UUID(id))
+	fmt.Println("token=", token, "err=", err)
+
+	// decode
+	appKey, err := auth.GetAppKey(token)
+	fmt.Println("appkey=", appKey, "err=", err)
+
+	if appKey != id {
+		t.Error("expecting", id, "but got", appKey)
+	}
+}
+
+func TestTokenExpiration(t *testing.T) {
+
+	id := UUID("1234")
+
+	auth := NewAuth([]byte("test"))
+	auth.TTLHours = 0
+
+	token, err := auth.GetAppToken(UUID(id))
+	fmt.Println("token=", token, "err=", err)
+
+	// decode
+	appKey, err := auth.GetAppKey(token)
+	fmt.Println("appkey=", appKey, "err=", err)
+
+	if err != ExpiredToken {
+		t.Error("expecting", ExpiredToken, "but got", err)
+	}
 
 }
