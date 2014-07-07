cb42423e0568524f438d54e007a04814492fb64a
dchung <dchung>
2014-07-07 16:35:51 -0700

    protobuf definitions

--- build-gen-proto.sh
+++ build-gen-proto.sh
@@ -6,4 +6,5 @@ export PATH=$(pwd)/third_party/bin:$PATH
 pushd proto
 protoc --go_out=../tally/ tally.proto
 protoc --go_out=../lighthouse/ lighthouse.proto
+protoc --go_out=../passport/ passport.proto
 popd
