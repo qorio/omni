771b7ab192b8ba19a5ab7c2f719c87a23e89e759
David Chung <dchung@qoriolabs.com>
2015-08-13 13:33:04 -0600

    Add logging json unmarshaler

--- rest/rest.go
+++ rest/rest.go
@@ -65,6 +65,15 @@ var (
 		return dec.Decode(typed)
 	}
 
+	json_logging_unmarshaler = func(body io.ReadCloser, typed interface{}) error {
+		buff, err := ioutil.ReadAll(body)
+		if err != nil {
+			return err
+		}
+		glog.V(100).Infoln("Unmarshal [", string(buff), "]")
+		return json.Unmarshal(buff, typed)
+	}
+
 	proto_marshaler = func(contentType string, resp http.ResponseWriter, any interface{}) error {
 		typed, ok := any.(proto.Message)
 		if !ok {
@@ -100,8 +109,8 @@ var (
 	}
 
 	unmarshalers = map[string]func(io.ReadCloser, interface{}) error{
-		"":                     json_unmarshaler,
-		"application/json":     json_unmarshaler,
+		"":                     json_logging_unmarshaler,
+		"application/json":     json_logging_unmarshaler,
 		"application/protobuf": proto_unmarshaler,
 		"text/plain":           string_unmarshaler,
 		"text/html":            nil,
